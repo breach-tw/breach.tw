528ab017236cc39e200a91d4aad1c683aa91a7ba
Jasper Yu <007seadog@gmail.com>
2019-08-01 19:21:37 +0000

    Fixed error msg for search API

--- api/search.php
+++ api/search.php
@@ -5,7 +5,7 @@ if(is_sha1($_GET['hash'])){
     $res = search($_GET['hash']);
 }else{
 	$res['status'] = '1';
-	$res['error'] = 'Field Error';
+	$res['error'] = '輸入格式錯誤';
 }
 
 echo json_encode($res);

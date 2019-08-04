753c1985e5293e2d1aa904c7940d783538e1dd7a
Jasper Yu <007seadog@gmail.com>
2019-08-04 13:56:53 +0000

    Fixed email verify status code

--- verify.php
+++ verify.php
@@ -10,14 +10,14 @@
     <section class="ts narrow container">
         <?php
             $status = verify_code($_GET['code']);
-            if($status == 0){
+            if($status == 1){
         ?>
         <div align="center" class="ts very positive padded segment">
             <p align="center">E-mail 驗證完成，會在發現個資外洩時第一時間通知您。</p>
             <button class="ts primary button" onclick="window.location='about.php'">查看更多關於我們的訊息</button>
         </div>
         <?php
-            }else if($status == 1){
+            }else if($status == 0){
         ?>
         <div align="center" class="ts very negative padded segment">
             <p align="center">E-mail 已驗證過。</p>

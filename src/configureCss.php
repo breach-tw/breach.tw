e9759a636c1edf2b0f5e16c7ca413864a86c9743
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-08-05 22:40:20 +0800

    Replace banner art with CSS/JS-driven artwork to reduce traffic

--- src/configureCss.php
+++ src/configureCss.php
@@ -1,3 +1,6 @@
 <link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="styles/master.css">
+<script src="js/particles.min.js"></script>
+<script>
+    particlesJS.load('banner-particle', 'styles/particles.json');
+</script>

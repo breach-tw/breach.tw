428d69074fb22fa6bd692aa974444e373a694154
Still Hsu <5843208+Still34@users.noreply.github.com>
2019-08-05 16:24:22 +0800

    Reconfigure meta components and switch to Spectre framework

---
+++ src/configureCss.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="https://unpkg.com/spectre.css/dist/spectre.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="styles/master.css">
--- src/configureMeta.php
+++ src/configureMeta.php
@@ -1,15 +1,8 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Open Graph implementation -->
 <meta name="description" content="本服務為針對台灣所設計的個資外洩追縱及查詢系統，可有效的查詢出個資外洩途徑。讓使用者了解自己是否存在於重大資安外洩事件清單中，且增加使用者之資訊安全素養。" />
-<meta property="og:title" content="<?=isset($title) ? $title.' | ':''?>台灣抓漏小天使" />
+<meta property="og:title" content="<?= isset($title) ? $title . ' | ' : '' ?>台灣抓漏小天使" />
 <meta property="og:type" content="website" />
 <meta property="og:description" content="本服務為針對台灣所設計的個資外洩追縱及查詢系統，可有效的查詢出個資外洩途徑。讓使用者了解自己是否存在於重大資安外洩事件清單中，且增加使用者之資訊安全素養。" />
 <meta property="og:image" content="https://breach.tw/images/logo.png" />
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-112153772-2"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-112153772-2');
-</script>
-

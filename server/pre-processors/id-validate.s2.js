d0572774f0acf8625afb7592dba64507b8791482
showgan <rexx0520@gmail.com>
2019-10-13 03:32:51 +0800

    我ㄉ i 不見ㄌ

--- server/pre-processors/id-validate.s2.js
+++ server/pre-processors/id-validate.s2.js
@@ -4,14 +4,21 @@ let A2 = new Array (0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5 );
 let Mx = new Array (9,8,7,6,5,4,3,2,1,1);
 
 function isValid(id) {
-    if (id.length != 10 || !tab.includes(id[0])) return false;
+    if (id.length != 10 ) return false;
+    
+    let i = tab.indexOf(id[0]);
+    
+    if (i === -1) {
+        return false;
+    }
 
     let sum = A1[i] + A2[i] * 9;
-    for ( let i = 1; i < 10; i++ ) {
+    for ( i = 1; i < 10; i++ ) {
       let v = parseInt( id[i] );
       if ( isNaN(v) ) return false;
       sum += v * Mx[i];
     }
+    
     return sum % 10 == 0;
 }
 
@@ -19,11 +26,11 @@ function filter(data, errorcb) {
     if (!data) return false;
     let [name, id] = data;
 
-    if (!isValid(id)) {
+    if (isValid(id)) {
+        return data;
+    } else {
         errorcb()
         return false;
-    } else {
-        return data;
     }
 }
 

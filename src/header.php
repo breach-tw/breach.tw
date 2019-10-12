e020d5524f991fe4161465fbf0d8c83d73512b3f
gnehs <jayuiop@gmail.com>
2019-10-12 16:18:24 +0800

    偵測 IE並建議使用新瀏覽器

--- src/header.php
+++ src/header.php
@@ -3,25 +3,28 @@ require 'config.php';
 ?>
 <!DOCTYPE HTML>
 <html>
+
 <head>
-<title><?=isset($title) ? $title.' | ':''?>台灣抓漏小天使</title>
-<meta charset="utf-8">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="/styles/index.css">
-<link rel="icon" href="/images/logo.svg">
-<link rel="icon" href="/images/logo.png">
-<script src="https://cdn.jsdelivr.net/gh/emn178/js-sha1/build/sha1.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php include 'src/og.php'; ?>
+	<title><?= isset($title) ? $title . ' | ' : '' ?>台灣抓漏小天使</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/styles/index.css">
+	<link rel="icon" href="/images/logo.svg">
+	<link rel="icon" href="/images/logo.png">
+	<script src="https://cdn.jsdelivr.net/gh/emn178/js-sha1/build/sha1.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+	<script src="/js/detectIE.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<?php include 'src/og.php'; ?>
 </head>
+
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark" >
+	<nav class="navbar navbar-expand-lg navbar-dark">
 		<div class="container">
-			<a class="navbar-brand" href="index.php"> 
+			<a class="navbar-brand" href="index.php">
 				<img src="/images/logo.svg" width="30" height="30" class="d-inline-block align-top" style="filter: invert(1)" alt="">
 				台灣抓漏小天使
 			</a>
@@ -31,26 +34,26 @@ require 'config.php';
 
 			<div class="collapse navbar-collapse" id="navbar">
 				<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-					<?php $uri = $_SERVER['REQUEST_URI'];?>
-					<li class="nav-item <?= $uri=='/search.php'?'active':''?>">
+					<?php $uri = $_SERVER['REQUEST_URI']; ?>
+					<li class="nav-item <?= $uri == '/search.php' ? 'active' : '' ?>">
 						<a class="nav-link" href="search.php">搜尋洩漏紀錄</a>
 					</li>
-					<li class="nav-item <?= $uri=='/subscribe.php'?'active':''?>">
+					<li class="nav-item <?= $uri == '/subscribe.php' ? 'active' : '' ?>">
 						<a class="nav-link" href="subscribe.php">訂閱外洩事件</a>
 					</li>
-					<li class="nav-item <?= $uri=='/manage_subscription.php'?'active':''?>">
+					<li class="nav-item <?= $uri == '/manage_subscription.php' ? 'active' : '' ?>">
 						<a class="nav-link" href="manage_subscription.php">查詢或取消訂閱</a>
 					</li>
-					<li class="nav-item <?= $uri=='/breaches.php'?'active':''?>">
+					<li class="nav-item <?= $uri == '/breaches.php' ? 'active' : '' ?>">
 						<a class="nav-link" href="breaches.php">重大外洩事件</a>
 					</li>
-					<li class="nav-item <?= $uri=='/stat.php'?'active':''?>">
+					<li class="nav-item <?= $uri == '/stat.php' ? 'active' : '' ?>">
 						<a class="nav-link" href="stat.php">網站統計數據</a>
 					</li>
-					<li class="nav-item <?= $uri=='/faq.php'?'active':''?>">
+					<li class="nav-item <?= $uri == '/faq.php' ? 'active' : '' ?>">
 						<a class="nav-link" href="faq.php">常見問題</a>
 					</li>
-					<li class="nav-item <?= $uri=='/about.php'?'active':''?>">
+					<li class="nav-item <?= $uri == '/about.php' ? 'active' : '' ?>">
 						<a class="nav-link" href="about.php">關於本服務</a>
 					</li>
 				</ul>
@@ -61,7 +64,7 @@ require 'config.php';
 						</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" style="padding-right: .5rem;padding-left: .5rem;" href="https://github.com/seadog007/breach.tw" target="_blank" >
+						<a class="nav-link" style="padding-right: .5rem;padding-left: .5rem;" href="https://github.com/seadog007/breach.tw" target="_blank">
 							<img src="/images/github.svg" width="24" height="24" class="d-inline-block align-top" style="filter: invert(1)" alt="">
 						</a>
 					</li>

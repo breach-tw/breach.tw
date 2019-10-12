da0c01cdfa76fc1588760e5399e8b806be3d31fd
Jasper Yu <007seadog@gmail.com>
2019-10-13 02:49:08 +0800

    Also match lowercase in s1-to-s2.js

--- server/pre-processors/s1-to-s2.js
+++ server/pre-processors/s1-to-s2.js
@@ -1,6 +1,6 @@
 function filter(line, errorcb = () => { }) {
     if (line) {
-        const match = line.match(/(.+?)(([A-Z]\d{3})?\d{6})/)
+        const match = line.match(/(.+?)(([A-Za-z]\d{3})?\d{6})/)
         if (match) {
             return [match[1], match[2]]
         } else {

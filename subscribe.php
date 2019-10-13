f4438484c32716e18cedcb2e99a769dd515a4f46
t510599 <t510599@gmail.com>
2019-10-13 20:34:51 +0800

    fixed wrong attribute name

--- subscribe.php
+++ subscribe.php
@@ -24,7 +24,7 @@
     <div class="padded container unSubForm" style="display:none">
         <h1 class="breach-title">取消訂閱表單</h1>
         <div class="alert alert-light text-center" role="alert">
-            此 E-mail <span class="color: green; font-weight: 700;">已訂閱</span>洩漏訊息，請輸入相關資訊來取消訂閱。
+            此 E-mail <span style="color: green; font-weight: 700;">已訂閱</span>洩漏訊息，請輸入相關資訊來取消訂閱。
         </div>
         <p>身分證字號會做去識別化後回傳，不會將資料本身傳送給我們。</p>
         <form id="unsubscribe_form">
@@ -47,7 +47,7 @@
     <div class="padded container subForm" style="display:none">
         <h1 class="breach-title">訂閱表單</h1>
         <div class="alert alert-light text-center" role="alert">
-            此 E-mail <span class="color: red; font-weight: 700;">尚未訂閱</span>洩漏訊息，請輸入相關資訊來訂閱。
+            此 E-mail <span style="color: red; font-weight: 700;">尚未訂閱</span>洩漏訊息，請輸入相關資訊來訂閱。
         </div>
         <p>身分證字號會做去識別化後回傳，不會將資料本身傳送給我們。<br/>訂閱後若發現大規模個資外洩時，且您在外洩清單內將會即時通知您。</p>
         <form id="subscribe_form">

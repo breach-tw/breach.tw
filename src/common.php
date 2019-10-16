1a7616673fde01354e644c8b572b34cefe3fbb15
Jasper Yu <007seadog@gmail.com>
2019-10-16 13:31:02 +0000

    Fixed id

--- src/common.php
+++ src/common.php
@@ -44,7 +44,7 @@ function get_leaked_items($source){
 
 function get_tags($source){
     global $db;
-    $stmt = $db->prepare("SELECT `id`,`name`,`class` FROM `source_tag` INNER JOIN `tag` `s` on `s`.`id` = `source_tag`.`tag` WHERE `source`=:source_id");
+    $stmt = $db->prepare("SELECT `source_tag`.`tag`,`name`,`class` FROM `source_tag` INNER JOIN `tag` `s` on `s`.`id` = `source_tag`.`tag` WHERE `source`=:source_id");
     $stmt->execute([
         'source_id' => $source
     ]);

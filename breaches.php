5932734304f90175284d248b9e570b7ce08bd379
Jasper Yu <007seadog@gmail.com>
2019-12-25 22:09:42 +0000

    Update breaches.php

--- breaches.php
+++ breaches.php
@@ -18,7 +18,7 @@
 		<h1 class="breach-title">資料來源 & 聲明</h1>
 		<p>本頁面僅顯示五千筆以上之外洩事件，剩餘較小（例如 Google 所得之名冊）將只會供受害者查詢。</p>
 		<p>本網站資料來源皆為匿名人士提供，且本網站並不保存其原始資料，只保存雜湊值以便查詢洩漏情況。<br />攻擊本網站並無法獲得資料，若攻擊屬實者本網站將依法訴諸法律行動。</p>
-		<p>若想匿名提供資源，可聯絡 <a href="mailto:admin@breach.tw">admin@breach.tw</a>，建議使用 PGP Key <a href="https://pgp.key-server.io/pks/lookup?op=get&fingerprint=on&search=0xF85EC40BC49D4040">C49D4040</a> 進行加密。</p>
+		<p>若想匿名提供資源，可參考 #TODO</p>
 		<h1 class="breach-title">重大外洩事件</h1>
 		<div class="breaches">
 			<?php foreach (get_major_breaches() as $_ => $val) { ?>

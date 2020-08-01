043731d6a9a1361bbf3993daafd51c4f36fd723c
Li-Heng Yu <007seadog@gmail.com>
2020-08-01 11:33:33 +0800

    Updated footer to 2020

--- src/footer.php
+++ src/footer.php
@@ -4,7 +4,7 @@
 
 <footer class="jumbotron jumbotron-fluid">
     <div class="container">
-        <p>台灣抓漏小天使<br/>Copyright © 2019 All Rights Reserved.</p>
+        <p>台灣抓漏小天使<br/>Copyright © 2020 All Rights Reserved.</p>
     </div>
 </footer>
 </body>
